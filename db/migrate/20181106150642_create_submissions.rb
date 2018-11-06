1dc2a0d97dc5f7a6a439acfd8676a945d2aadc89
EC2 Default User <ec2-user@ip-172-31-15-36.us-east-2.compute.internal>
2018-11-06 15:08:54 +0000

    Generate and run Submissions migration

---
+++ db/migrate/20181106150642_create_submissions.rb
@@ -0,0 +1,10 @@
+class CreateSubmissions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :submissions do |t|
+      t.string :title
+      t.text :text
+
+      t.timestamps
+    end
+  end
+end
