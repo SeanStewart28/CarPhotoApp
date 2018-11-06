d171c50a5a4c7db0d2ceccdd50403377ba576e68
EC2 Default User <ec2-user@ip-172-31-15-36.us-east-2.compute.internal>
2018-11-06 15:21:41 +0000

    complete create and show

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,8 +1,20 @@
 class SubmissionsController < ApplicationController
+    def show 
+        @submission = Submission.find(params[:id])
+    end
+    
     def new
     end
     
     def create
-        render plain: params[:submission].inspect
+        @submission = Submission.new(submission_params)
+        
+        @submission.save
+        redirect_to @submission
     end
 end
+
+private 
+    def submission_params
+        params.require(:submission).permit(:title, :text)
+    end
