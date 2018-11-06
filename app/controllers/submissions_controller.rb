02e4b6c435262da3ecd0b5168a653d866d4c76df
EC2 Default User <ec2-user@ip-172-31-15-36.us-east-2.compute.internal>
2018-11-06 15:27:56 +0000

    list all submissions

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,4 +1,8 @@
 class SubmissionsController < ApplicationController
+    
+    def index 
+        @submissions = Submission.all
+    end
     def show 
         @submission = Submission.find(params[:id])
     end
