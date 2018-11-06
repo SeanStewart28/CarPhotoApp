46397ff00722d270c4e0a5a7ed1e25e82c0d4138
EC2 Default User <ec2-user@ip-172-31-15-36.us-east-2.compute.internal>
2018-11-06 15:48:42 +0000

    add error messages for title validations

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -3,18 +3,23 @@ class SubmissionsController < ApplicationController
     def index 
         @submissions = Submission.all
     end
+    
     def show 
         @submission = Submission.find(params[:id])
     end
     
     def new
+        @submission = Submission.new
     end
     
     def create
         @submission = Submission.new(submission_params)
         
-        @submission.save
-        redirect_to @submission
+        if @submission.save
+            redirect_to @submission
+        else
+            render 'new'
+        end
     end
 end
 
